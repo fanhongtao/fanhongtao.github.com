9aab74376f4923f6c54a9b77f1b5d80d8f255c89
Fan Hongtao <fanhongtao@gmail.com>
2013-09-15 20:26:43 +0800

    Update using-mvn

--- attachments/prepare_maven.sh
+++ attachments/prepare_maven.sh
@@ -4,35 +4,42 @@
 # This file is prepare Maven enviroment after install Maven.
 # @author Fan Hongtao <fanhongtao@gmail.com>
 
+function exec() {
+    echo
+    echo "Exec: ##### $*"
+    $*
+}
+
 old_path=`pwd`
 
-mkdir maven
-cd maven
+exec mkdir maven
+exec cd maven
 
 # Execute common Maven commands
-mvn archetype:generate -B -DarchetypeArtifactId=maven-archetype-quickstart -DgroupId=org.fanhongtao -DartifactId=test1 -Dversion=1.0-SNAPSHOT
-cd test1
-mvn help:describe -Dplugin=archetype
+exec mvn archetype:generate -B -DarchetypeArtifactId=maven-archetype-quickstart -DgroupId=org.fanhongtao -DartifactId=test1 -Dversion=1.0-SNAPSHOT
+exec cd test1
+exec mvn help:describe -Dplugin=archetype
 
-mvn dependency:analyze
-mvn dependency:copy-dependencies 
-mvn dependency:go-offline
-mvn dependency:list
+exec mvn dependency:analyze
+exec mvn dependency:copy-dependencies 
+exec mvn dependency:go-offline
+exec mvn dependency:list
 
-mvn help:help
-mvn help:describe -Dplugin=archetype -Ddetail
+exec mvn help:help
+exec mvn help:describe -Dplugin=archetype -Ddetail
 
-mvn test
-mvn javadoc:jar
-mvn source:jar
+exec mvn test
+exec mvn javadoc:jar
+exec mvn source:jar
+exec mvn install
 
 # Download jars, and their sources,  used by daily work
-cd ../
-mkdir default
-cd default
+exec cd ../
+exec mkdir default
+exec cd default
 curl https://raw.github.com/fanhongtao/fanhongtao.github.com/master/attachments/default_pom.xml > pom.xml
-mvn dependency:sources
+exec mvn dependency:sources
 
 # return 
-cd "$old_path"
-rm -rf maven
+exec cd "$old_path"
+exec rm -rf maven
